6f9d42c2a351330c63dec45ef516425a95ed75b5
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-03-21 00:09:22 +0900

    getInstalledApplications と getInstalledPackages の結果を出力する

--- PackageVisibilitySample/app/src/main/java/com/github/mag0716/packagevisibilitysample/MainActivity.kt
+++ PackageVisibilitySample/app/src/main/java/com/github/mag0716/packagevisibilitysample/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.github.mag0716.packagevisibilitysample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "PackageVisibility"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            getInstalledApplications()
+        }
+    }
+
+    fun getInstalledApplications() {
+        val installedApplications = packageManager.getInstalledApplications(0)
+        val installedPackages = packageManager.getInstalledPackages(0)
+
+        Log.d(TAG, "installedApplications: $installedApplications")
+        Log.d(TAG, "installedPackages: $installedPackages")
     }
 }
