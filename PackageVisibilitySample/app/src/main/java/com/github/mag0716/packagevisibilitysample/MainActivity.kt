df829d6aee7fc55f09b7ce0f23960e03d45d601b
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-03-22 21:52:05 +0900

    連携先のアプリを疑似するモジュールを追加

--- PackageVisibilitySample/app/src/main/java/com/github/mag0716/packagevisibilitysample/MainActivity.kt
+++ PackageVisibilitySample/app/src/main/java/com/github/mag0716/packagevisibilitysample/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.github.mag0716.packagevisibilitysample
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -15,14 +17,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button.setOnClickListener {
+        button1.setOnClickListener {
             getInstalledApplications()
+        }
+        button2.setOnClickListener {
+            val intent = Intent().apply {
+                setClassName(
+                    "com.github.mag0716.packagevisibility.target",
+                    "com.github.mag0716.packagevisibility.target.MainActivity"
+                )
+            }
+            startActivity(intent)
         }
     }
 
     fun getInstalledApplications() {
-        val installedApplications = packageManager.getInstalledApplications(0)
-        val installedPackages = packageManager.getInstalledPackages(0)
+        val flags = PackageManager.GET_ACTIVITIES or PackageManager.GET_SERVICES
+        val installedApplications = packageManager.getInstalledApplications(flags)
+        val installedPackages = packageManager.getInstalledPackages(flags)
 
         Log.d(TAG, "installedApplications: ")
         for (installedApplication in installedApplications) {
