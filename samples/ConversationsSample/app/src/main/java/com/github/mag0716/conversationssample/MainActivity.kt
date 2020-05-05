c981a746182b744c94c930e54b8c905178d3d48d
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-05-05 21:36:54 +0900

    コメント追加

--- samples/ConversationsSample/app/src/main/java/com/github/mag0716/conversationssample/MainActivity.kt
+++ samples/ConversationsSample/app/src/main/java/com/github/mag0716/conversationssample/MainActivity.kt
@@ -6,12 +6,22 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.Person
 import androidx.core.graphics.drawable.IconCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ * Best practices(https://developer.android.com/guide/topics/ui/bubbles#best_practices)
+ *
+ * * 継続中のコミュニケーションのためや、ユーザが明示的に要求した場合のみに利用すること
+ * * Bubble はユーザが無効化することも可能なので、無効化されたケースも想定しておくこと
+ * * Bubble で開いた画面もタスクを保持しているので極力シンプルな機能のみ利用できるようにしておくこと
+ *
+ */
 class MainActivity : AppCompatActivity() {
 
     companion object {
@@ -34,8 +44,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun receiveMessage(message: String) {
-        textView.text = "${textView.text}\nYou:$message"
-        sendNotification(message)
+        Handler(Looper.myLooper()!!).postDelayed({
+            textView.text = "${textView.text}\nYou:$message"
+            sendNotification(message)
+        }, 5000)
     }
 
     private fun sendNotification(message: String) {
@@ -70,6 +82,13 @@ class MainActivity : AppCompatActivity() {
         notificationManager.createNotificationChannel(channel)
     }
 
+    /**
+     * Bubble の表示タイミング
+     *
+     * * MessagingStyle を利用していて、Person が追加されている
+     * * CATEGORY_CALL の Service.startForeground から呼ばれ、Person が追加されている
+     * * アプリがフォアグラウンドの時に Notification が送信された場合
+     */
     private fun createBubbleMetaData(): NotificationCompat.BubbleMetadata {
         val intent = Intent(this, MainActivity::class.java)
         val bubbleIntent = PendingIntent.getActivity(this, 0, intent, 0)
@@ -77,6 +96,8 @@ class MainActivity : AppCompatActivity() {
             .setDesiredHeight(600)
             .setIcon(IconCompat.createWithResource(this, R.drawable.ic_android))
             .setIntent(bubbleIntent)
+            //.setAutoExpandBubble(true) // 自動的に Bubble を開く。ユーザ操作きっかけ時に利用することが推奨されている
+            //.setSuppressNotification(true) // Bubble 生成時に Notification の表示を抑制する。一般的にはユーザ操作で Bubble が生成された時に利用する
             .build()
     }
 }
