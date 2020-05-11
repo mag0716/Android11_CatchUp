83634b4d87a16fe7ab05e0c5d24305908ae05e86
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-05-11 21:17:27 +0900

    バックグラウンドでの位置情報取得権限が必要な理由を説明するための Dialog を表示する

--- samples/OneTimePermissionSample/app/src/main/java/com/github/mag0716/onetimepermissionsample/MainActivity.kt
+++ samples/OneTimePermissionSample/app/src/main/java/com/github/mag0716/onetimepermissionsample/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.github.mag0716.onetimepermissionsample
 
 import android.Manifest
+import android.annotation.TargetApi
+import android.app.Dialog
+import android.content.Context
+import android.content.DialogInterface
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    ExplainBackgroundLocationRequirementDialog.OnClickListener {
 
     companion object {
         private const val TAG = "OneTimePermission"
@@ -28,7 +35,11 @@ class MainActivity : AppCompatActivity() {
         button2.setOnClickListener {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                 if (isGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
-                    requestPermission(arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION))
+                    val dialogFragment = ExplainBackgroundLocationRequirementDialog()
+                    dialogFragment.show(
+                        supportFragmentManager,
+                        ExplainBackgroundLocationRequirementDialog::class.java.simpleName
+                    )
                 } else {
                     Toast.makeText(
                         this,
@@ -90,4 +101,40 @@ class MainActivity : AppCompatActivity() {
 
     private fun isGranted(feature: String) =
         ContextCompat.checkSelfPermission(this, feature) == PackageManager.PERMISSION_GRANTED
+
+    override fun onOkClicked() {
+        requestPermission(arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION))
+    }
+}
+
+@TargetApi(30)
+class ExplainBackgroundLocationRequirementDialog : DialogFragment() {
+
+    interface OnClickListener {
+        fun onOkClicked()
+    }
+
+    lateinit var listener: OnClickListener
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        listener = context as OnClickListener
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val activity = requireActivity()
+        val packageManager = activity.application.packageManager
+        val builder = AlertDialog.Builder(activity)
+        builder.setMessage(
+            """
+            機能を利用するためアプリ利用中以外でも位置情報の取得を行う必要があります。
+            設定画面で ${packageManager.backgroundPermissionOptionLabel} を選択して、位置情報の取得を許可してください。
+            """.trimIndent()
+        )
+            .setNegativeButton("キャンセル", null)
+            .setPositiveButton("OK") { _: DialogInterface, _: Int ->
+                listener.onOkClicked()
+            }
+        return builder.create()
+    }
 }
