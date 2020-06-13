bf78e457a0f3354d284101b798ca3efefbcec0dd
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-06-13 22:53:35 +0900

    エラー処理を雑に実装

--- samples/ControlExternalDevicesSample/app/src/main/java/com/github/mag0716/controlexternaldevicessample/DeviceSettingFragment.kt
+++ samples/ControlExternalDevicesSample/app/src/main/java/com/github/mag0716/controlexternaldevicessample/DeviceSettingFragment.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputLayout
 
 class DeviceSettingFragment : Fragment() {
+
+    private lateinit var deviceNameInput: TextInputLayout
+    private lateinit var deviceLocationInput: TextInputLayout
+    private lateinit var settingButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,5 +24,29 @@ class DeviceSettingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        deviceNameInput = view.findViewById(R.id.deviceNameInputLayout)
+        deviceLocationInput = view.findViewById(R.id.deviceLocationInputLayout)
+        settingButton = view.findViewById(R.id.settingButton)
+        settingButton.setOnClickListener {
+            deviceNameInput.error = null
+            deviceLocationInput.error = null
+
+            val deviceName = deviceNameInput.editText?.text.toString()
+            val deviceLocation = deviceLocationInput.editText?.text.toString()
+            if (deviceName.isBlank() || deviceLocation.isBlank()) {
+                if (deviceName.isBlank()) {
+                    deviceNameInput.error = "入力必須"
+                }
+                if (deviceLocation.isBlank()) {
+                    deviceLocationInput.error = "入力必須"
+                }
+            } else {
+                settingDevice(deviceName, deviceLocation)
+            }
+        }
+    }
+
+    private fun settingDevice(name: String, location: String) {
+        // TODO: DB に保存
     }
 }
