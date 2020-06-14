b0461e33a5f92e032fe6e7637c7d5cfbf6cd37a2
Masashi Kishimoto <mkishimoto0716@gmail.com>
2020-06-14 21:07:50 +0900

    Room でデバイス情報を管理するための定義を追加

--- samples/ControlExternalDevicesSample/app/src/main/java/com/github/mag0716/controlexternaldevicessample/model/Device.kt
+++ samples/ControlExternalDevicesSample/app/src/main/java/com/github/mag0716/controlexternaldevicessample/model/Device.kt
@@ -1,7 +1,11 @@
 package com.github.mag0716.controlexternaldevicessample.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "devices")
 data class Device(
-    val id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int,
     val name: String,
     val placeLocation: String
 )
---
+++ samples/ControlExternalDevicesSample/app/src/main/java/com/github/mag0716/controlexternaldevicessample/repository/DeviceRepository.kt
@@ -0,0 +1,20 @@
+package com.github.mag0716.controlexternaldevicessample.repository
+
+import com.github.mag0716.controlexternaldevicessample.model.Device
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class DeviceRepository(private val deviceDao: DeviceDao) {
+
+    suspend fun loadDevices(): List<Device> = withContext(Dispatchers.IO) {
+        deviceDao.loadDevices()
+    }
+
+    suspend fun loadDevice(id: Int): Device = withContext(Dispatchers.IO) {
+        deviceDao.loadDeviceById(id)
+    }
+
+    suspend fun addDevice(device: Device) = withContext(Dispatchers.IO) {
+        deviceDao.insertDevices(device)
+    }
+}
